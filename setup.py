51c29e5e956316d3e68361450b0e3ef4c3d0fab8
Rob Gura <robert_gura@yahoo.com>
2018-08-17 18:43:50 -0500

    Logs failures to appengine logs

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 utility functions to share among models
 """
 import traceback
+import logging
 
 #pylint: disable=import-error
 from werkzeug.exceptions import default_exceptions
@@ -13,6 +14,10 @@ def setup(app):
         rv = error.__dict__
         rv['type'] = str(type(error).__name__)
         rv['stack'] = traceback.format_exc().split('\n')
+
+        logging.error('invalid usage %s', rv['type'])
+        logging.error('invalid usage\n%s', rv['stack'])
+
         return jrg.util.to_json(rv), 499
 
     # got this extra bit from https://stackoverflow.com/a/29332131
